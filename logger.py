f21c6d4b42ad3d2a93be2285276117ea4d3fce80
LinuxV3 <linuxv000@gmail.com>
2025-02-07 23:19:38 +0330

    add some comments to explain code

--- logger.py
+++ logger.py
@@ -1,8 +1,8 @@
 import logging
 
-log_format = '%(asctime)s -> %(levelname)s: %(message)s'
+log_format = '%(asctime)s -> %(levelname)s: %(message)s' # For example '2022-07-09 14:30:00,123 -> INFO: This is a test'
 date_format = '%Y-%m-%d %H:%M:%S'
-logger = logging.getLogger("Core")
+logger = logging.getLogger("AiClient")
 logger.setLevel(logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG, format=log_format, datefmt=date_format)
 log_types = {"debug": logger.debug,
@@ -14,7 +14,7 @@ log_types = {"debug": logger.debug,
              'log': logger.log}
 
 
-def log(dest: str, log_type='INFO'):
+def log(dest: str, log_type='INFO') -> None:
     log_type = log_type.lower()
     if log_type in log_types:
         log_types[log_type](dest)
